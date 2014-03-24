76aeb6dd6f36446ea9cff94f1c1d1ad4cc71d9e6
brain-deaf <patrickoneil.music@gmail.com>
2014-03-23 18:56:15 -0700

    simplified pyplayer.pyx

--- sampler/pygst/pyplayer.pyx
+++ sampler/pygst/pyplayer.pyx
@@ -10,12 +10,6 @@ cdef extern from "player.h":
         void set_volume_track2(double _volume)
         void listen(void(*f)(double, vector[unsigned char]*, void*), void* userdata) nogil
 
-cdef extern from "midi.h":
-    cdef cppclass MidiInput:
-        void mycallback(double deltatime, vector[unsigned char]* message, void* userdata) with gil
-        int midi_listen(void(*)(double, vector[unsigned char]*, void*), void* userdata) nogil
-        int midi_listen2(void* userdata) nogil
-
 cdef void midi_init(Player* ob, void* function) nogil:
     with nogil:
         ob.listen(callback, function)
@@ -23,17 +17,10 @@ cdef void midi_init(Player* ob, void* function) nogil:
 cdef void callback(double deltatime, vector[unsigned char]* message, void* userdata) with gil:
     (<object>userdata)(message[0])	
 
-cdef class MidiListen:
-    cdef MidiInput* thisptr
-    def __cinit__(self):
-        self.thisptr = new MidiInput()
-
 cdef class PyPlayer:
     cdef Player *thisptr
     def __cinit__(self):
         self.thisptr = new Player()
-    def run(self):
-        self.thisptr.start_main()
     def play_sample(self, sample_name):
         self.thisptr.play_sample(sample_name)
     def set_volume(self, _volume):
