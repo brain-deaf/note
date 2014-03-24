fa6b76490e39c614ce43b9be4e8bfe4a6236d79a
abresee <bresee.al@gmail.com>
2014-03-23 22:07:37 -0400

    delete old shit

--- old_core/Packet_py.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/python.hpp>
-#include "Packet.h"
-
-using namespace boost::python;
-
-void export_Packet() {
-    class_<Packet>("Packet", init<Packet::size_type>())
-        .def(init<Packet::size_type, Sample>());
-}
